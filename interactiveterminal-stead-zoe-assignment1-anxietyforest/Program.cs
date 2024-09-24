a99d463c9d0cda32f45ab3eba50c457dffb3360a
liamstead05 <000952741@mohawkcollege.ca>
2024-09-23 23:38:44 -0400

    Add dialogue Add start up screen Add first part

--- interactiveterminal-stead-zoe-assignment1-anxietyforest/Program.cs
+++ interactiveterminal-stead-zoe-assignment1-anxietyforest/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+
+internal class Program
+{ 
+    private static void Main(string[] args)
+    {
+        // Introduce the player to the game of "Anxiety Forest"
+        Console.WriteLine("Welcome to the Anxiety Forest!");
+        Console.WriteLine("Type \"start\" to begin the game!");
+
+        // Get a response from the player
+        string playerInput;
+        playerInput = Console.ReadLine();
+
+        // Compare the events of starting up the game
+        if (playerInput == "Start");
+        {
+            // Start the game - dialogue
+            Console.WriteLine("You are Lexi, a 20 year old girl.");
+            Console.WriteLine("You love socializing with people, but you've been having trouble doing so because of your anxiety.");
+            Console.WriteLine("You also love going on hikes, and you tell yourself that today is the day to overcome your anxiety.");
+            Console.WriteLine("You pack up your stuff and make your way to the Forest of Destiny in the town of New Athens");
+        }
+        else if (playerInput == "Quit") ;
+        {
+            // End the program
+            Console.WriteLine("Have a nice day!");
+        }
+        else
+        {
+            // Let the player know that what they typed is invalid
+            Console.WriteLine($"Your response is \"{playerInput}\" is not valid.");
+        }
+    }
+}
+
